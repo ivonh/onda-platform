681dbd0c19015cc6707577bced1635d2f3c45cb1
Ivon Harris <87586885+ivonh@users.noreply.github.com>
2026-03-04 15:15:36 +1000

    Add PWA support: index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import * as serviceWorkerRegistration from "@/serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +10,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    if (registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      window.location.reload();
+    }
+  },
+  onSuccess: () => {
+    console.log('Onda is ready for offline use.');
+  }
+});
